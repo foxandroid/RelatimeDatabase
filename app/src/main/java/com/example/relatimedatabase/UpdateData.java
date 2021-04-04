dd2d1e6c03ba6e4177ef7b5f47c3590453b87a5a
robin.singla <robinformal@gmail.com>
2021-04-04 17:46:33 +0530

    Updata Data File Entered

---
+++ app/src/main/java/com/example/relatimedatabase/UpdateData.java
@@ -0,0 +1,14 @@
+package com.example.relatimedatabase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UpdateData extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_data);
+    }
+}
